7d30d58f560675a5e41f6303d1cb07534090c8aa
zoom-kris-anderson <36420794+zoom-kris-anderson@users.noreply.github.com>
2018-10-04 18:13:00 -0400

    Install google python API (#6)
    * Install google python API
    
    * add sd sender utility
    
    * add utility file directive
    
    * Update init.pp

---
+++ files/utils/sdsend.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python -u
+
+from google.cloud import monitoring_v3
+
+import time
+import argparse
+import time
+import os
+import sys
+
+for line in sys.stdin:
+  shell_input = int(line.strip())
+
+parser = argparse.ArgumentParser(description='Ship custom metrics directly to StackDriver')
+client = monitoring_v3.MetricServiceClient()
+
+parser.add_argument('-p', '--project', help="Project for StackDriver Metric", required=true)
+parser.add_argument('-c', '--custom', help="Example: custom.googleapis.com/example_top_level/metric1", required=True)
+
+args = parser.parse_args()
+
+project_name = client.project_path(args.project)
+
+series = monitoring_v3.types.TimeSeries()
+#series.metric.type = 'custom.googleapis.com/testmetric/metric1'
+series.metric.type = args.custom
+
+# Get values from stdin piped value
+point = series.points.add()
+point.value.double_value = shell_input
+
+now = time.time()
+point.interval.end_time.seconds = int(now)
+point.interval.end_time.nanos = int(
+    (now - point.interval.end_time.seconds) * 10**9)
+
+client.create_time_series(project_name, [series])
+
+print "Successfully wrote time series. %s %d" % (series.metric.type, point.value.double_value)
--- manifests/init.pp
+++ manifests/init.pp
@@ -43,6 +43,7 @@ class stackdriver (
   $managerepo = true,
   $service_ensure = 'running',
   $service_enable = true,
+  $google_python_api = false,
   $gcm = false,
 
   $svc = $gcm ? {
@@ -97,6 +98,59 @@ class stackdriver (
     require        => Class[$cclass],
   }
   include $sclass
+
+  # Ensure Google's Python Packages are Installed
+  # This requires >=python2.7 so CentOS 6 will need to use scl
+  if $google_python_api {
+  
+    file { '/etc/stackdriver/utils/':
+      ensure => 'directory',
+      owner  => 'root',
+      group  => 'root'
+    }
+    file { '/etc/stackdriver/utils/sdsend.py':
+      ensure  => 'present',
+      source  => 'puppet:///modules/stackdriver/utils/sdsend.py',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755'
+    }
+  
+    if fact('os.name') == "CentOS" and
+       fact('os.release.major') == "6" {
+
+       if ! defined( Package[ 'centos-release-scl' ] ) {
+         package { [ 'centos-release-scl', 'python27' ]:
+           ensure => 'installed'
+         }
+       }
+       exec { 'pip install google-api-python-client':
+         command  => "/usr/bin/scl enable python27 'pip install google-api-python-client'",
+         unless   => "/usr/bin/scl enable python27 'pip show google-api-python-client'"
+       }
+       exec { 'pip install google-cloud-monitoring':
+         command  => "/usr/bin/scl enable python27 'pip install google-cloud-monitoring'",
+         unless   => "/usr/bin/scl enable python27 'pip show google-cloud-monitoring'"
+       }
+    }
+    if fact('os.name') == "CentOS" and
+       fact('os.release.major') == "7" {
+
+       if ! defined(Package[ 'python-pip' ]) {
+         package { [ 'python-pip' ]:
+           ensure => 'installed'
+         }
+       }
+       exec { 'pip install google-api-python-client':
+         command  => "pip install google-api-python-client",
+         unless   => "pip show google-api-python-client"
+       }
+       exec { 'pip install google-cloud-monitoring':
+         command  => "pip install google-cloud-monitoring",
+         unless   => "pip show google-cloud-monitoring"
+       }
+    }
+  }
 
   # Array of Plugins to load (optional)
   $plugins = hiera_array("${name}::plugins", [])
